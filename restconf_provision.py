849def7df5e653470690bc47d3ae0b4ba2d428bc
Marco Zollinger <marco@freelabs.space>
2019-09-28 18:34:29 +0200

    start parsing yaml config for provisioning

--- restconf_provision.py
+++ restconf_provision.py
@@ -29,26 +29,27 @@ def init_logger():
 
 def main():
     logger.info(f"Loading device configurations...")
-    devices = load_devices()
+    devices = load_config()
     for device in devices:
         logger.info(f"Found configuration for device {device['devicename']} on address {device['ip_address']}")
     input("Proceed with provisioning these devices?")
 
-    # THIS IS A TEST ONLY FOR BGP XML
-    endpoint_url = f"https://{device['ip_address']}/restconf/data/Cisco-IOS-XE-native:native/router/bgp=4"
-    env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
-    template = env.get_template('templates/bgp.j2')
-#    print(endpoint_url)
-#    print(template.render())
-    response = requests.put(url=endpoint_url,
-                       data=template.render(),
-                       auth=(device['username'], device['password']),
-                       verify=False
-                       )
-    if response:
-        print("Provisioning successful")
-    else:
-        print(f"Provisioning failed with HTTP error {response.status_code}")
+
+    for device in devices:
+        for section in device['configuration']:
+            print(section['section'])
+            device_url = f"https://{device['ip_address']}/restconf/data/Cisco-IOS-XE-native:{section['endpoint_url']}"
+            env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+            template = env.get_template(f"templates/{section['section']}.j2")
+            response = requests.put(url=device_url,
+                                    data=template.render(),
+                                    auth=(device['username'], device['password']),
+                                    verify=False
+                                   )
+            if response:
+                print("Provisioning successful")
+            else:
+                print(f"Provisioning failed with HTTP error {response.status_code}")
 
 
 if __name__ == '__main__':
